824f0dc40c8f9496e3b9d09dbdea43566d8b4b3c
aleks <aleksei.petaikin@gmail.com>
2022-07-23 17:39:03 +0500

    parser start

---
+++ bot.py
@@ -0,0 +1,21 @@
+
+TOKEN = '5435921460:AAEijUBYOPl1ePVtE-MkUAHu4QbOj5Ypppo'
+
+import telebot
+
+bot = telebot.TeleBot(TOKEN) # You can set parse_mode by default. HTML or MARKDOWN
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
